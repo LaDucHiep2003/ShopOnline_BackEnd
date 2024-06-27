40932567188765292159d5e95bdeedf28e393be7
LaDucHiep2003 <duchiep045@gmail.com>
2024-06-27 11:07:16 +0700

    chuc nang co ban

--- Controllers/category.controller.js
+++ Controllers/category.controller.js
@@ -1,11 +1,86 @@
 
 const productCategory = require("../Models/category.model")
+const searchHelper = require("../helper/search.helper")
+// [GET] /admin/product-category
 module.exports.index = async (req, res) => {
     const find = {
         deleted: false
     }
 
-    const products = await productCategory.find(find)
+    // Status
+    if(req.query.status){
+        find.status = req.query.status
+    }
+
+    // Search
+    const objectSearch = searchHelper(req.query)
+
+    if (objectSearch.regex) {
+        find.name = objectSearch.regex
+    }
+
+    // Sort
+    const Sort = {
+
+    }
+
+    if(req.query.sortKey && req.query.sortValue){
+        Sort[req.query.sortKey] = req.query.sortValue
+    }else{
+        Sort.position = "desc"
+    }
+
+    const products = await productCategory.find(find).sort(Sort)
 
     res.json(products)
+}
+
+//[GET] /admin/product-category/change-status/:id
+module.exports.changeStatus = async (req, res) => {
+    try {
+        const id = req.params.id
+        const status = req.body.status
+
+        await productCategory.updateOne({
+            _id: id
+        }, {
+            status: status
+        })
+
+        res.json({
+            code: 200,
+            message: "Cập nhật trạng thái thành công"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Không tồn tại"
+        })
+    }
+
+}
+
+// [GET] /admin/product-category/delete/:id
+module.exports.delete = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        console.log(id);
+        await productCategory.updateOne({
+            _id: id
+        }, {
+            deleted: true,
+        })
+
+        res.json({
+            code: 200,
+            message: "Xóa thành công! ",
+        })
+
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi"
+        })
+    }
 }
--- routers/category.route.js
+++ routers/category.route.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const controller = require('../Controllers/category.controller')
 
 router.get('/',controller.index)
+router.patch('/change-status/:id', controller.changeStatus)
+router.patch('/delete/:id', controller.delete)
 
 module.exports = router
